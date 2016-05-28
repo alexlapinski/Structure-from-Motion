566a84f55afbb8807ce3af4f6851fa8a2ea8b04b
Alex Lapinski <contact@alexlapinski.name>
2016-05-28 16:07:42 -0400

    Finished solve for Q

--- p3_skeleton.py
+++ p3_skeleton.py
@@ -57,30 +57,66 @@ def solve_Q(i_hat, j_hat):
     """Solve for Q that satisfies the unit-vector and orthogonality constraints
     on ih and jh."""
 
-    print i_hat.shape
-    print j_hat.shape
-
     if i_hat.shape[0] != j_hat.shape[0] or i_hat.shape[1] != j_hat.shape[1]:
         raise ValueError("i_hat's shape must equal j_hat's shape")
 
     num_frames = i_hat.shape[0]
 
     # Build A and b
-    #A = ?
+    A = np.zeros((3 * num_frames, 6))
 
+    offset = 0
+    for frame in xrange(num_frames):
+        i_hat_row = i_hat[frame]
+        j_hat_row = j_hat[frame]
 
+        # There probably is a 'better' way to compute each row segment
+        # but this works for now
+        elem_1_1 = i_hat_row[0]**2
+        elem_1_2 = 2 * i_hat_row[0] * i_hat_row[1]
+        elem_1_3 = 2 * i_hat_row[0] * i_hat_row[2]
+        elem_1_4 = i_hat_row[1]**2
+        elem_1_5 = 2 * i_hat_row[1] * i_hat_row[2]
+        elem_1_6 = 2 * i_hat_row[2]**2
+        row_1 = [elem_1_1, elem_1_2, elem_1_3, elem_1_4, elem_1_5, elem_1_6]
 
+        elem_2_1 = j_hat_row[0]**2
+        elem_2_2 = 2 * j_hat_row[0] * j_hat_row[1]
+        elem_2_3 = 2 * j_hat_row[1] * j_hat_row[2]
+        elem_2_4 = j_hat_row[1]**2
+        elem_2_5 = 2 * j_hat_row[1] * j_hat_row[2]
+        elem_2_6 = j_hat_row[2]**2
+        row_2 = [elem_2_1, elem_2_2, elem_2_3, elem_2_4, elem_2_5, elem_2_6]
 
-    b = np.ones((num_frames, 1))
+        elem_3_1 = i_hat_row[0] * j_hat_row[0]
+        elem_3_2 = i_hat_row[0] * j_hat_row[1] + i_hat_row[1] * j_hat_row[0]
+        elem_3_3 = i_hat_row[0] * j_hat_row[2] + i_hat_row[2] * j_hat_row[0]
+        elem_3_4 = i_hat_row[1] * j_hat_row[1]
+        elem_3_5 = i_hat_row[1] * j_hat_row[2]
+        elem_3_6 = i_hat_row[2] * j_hat_row[2]
+        row_3 = [elem_3_1, elem_3_2, elem_3_3, elem_3_4, elem_3_5, elem_3_6]
+
+        A[offset:offset + 3] = [row_1, row_2, row_3]
+        offset += 3
+
+    b = np.ones((3 * num_frames))
     # Every 3rd row is equal to zero
     b[2::3] = 0
 
     # Solve for c via least squares.
+    result = np.linalg.lstsq(A, b)
+    c = result[0]
 
     # Form C from c
+    C = np.zeros((3, 3))
+    C[0] = [c[0], c[1], c[2]]
+    C[1] = [c[1], c[3], c[4]]
+    C[2] = [c[2], c[4], c[5]]
 
     # Solve for Q and return it.
-    return None
+    Q = np.linalg.cholesky(C)
+
+    return Q
 
 
 def sfm(points):
@@ -93,8 +129,6 @@ def sfm(points):
     normalized_W_matrix = build_W(points)
     R_hat, S_hat = compute_RS(normalized_W_matrix)
 
-    print "R_hat's shape", R_hat.shape
-
     # Get ih/jh from Rh and use them to find Q.
     i_hat = R_hat[:num_frames, :]
     j_hat = R_hat[num_frames:, :]
@@ -102,6 +136,7 @@ def sfm(points):
     Q = solve_Q(i_hat, j_hat)
 
     # Use Q, Rh, and Sh to get R and S.
+
 
     # Extract the rotation matrices from R and put them into a list, one R per
     # image.
