82bacd19cb4d8f167fb4e5a1204f92f21e3ae7db
Alex Lapinski <contact@alexlapinski.name>
2016-05-30 12:47:09 -0400

    Got close to exact reprojections

--- p3_skeleton.py
+++ p3_skeleton.py
@@ -9,28 +9,20 @@ def build_W(points, debug=False):
     num_frames = points.shape[0]
     num_points = points.shape[1]
 
-    num_rows = 2 * num_frames
-    num_cols = num_points
-    normalized_W = np.zeros((num_rows, num_cols))
+    w_rows = 2 * num_frames
+    w_cols = num_points
+    normalized_W = np.zeros((w_rows, w_cols))
 
     # Points are in the format X Y
     for f in xrange(len(points)):
         frame = points[f]
-        #x_total = 0
-        #y_total = 0
-        #for point in frame:
-        #    x_total += point[0]
-        #    y_total += point[1]
-        #x_average = x_total / len(frame)
-        #y_average = y_total / len(frame)
-        mean = frame.mean(0)
-
-        print mean
+        center = frame.mean(0)
 
         for p in xrange(len(frame)):
             point = frame[p]
-            normalized_W[f][p] = point[0] - mean[0]
-            normalized_W[f + num_frames][p] = point[1] - mean[1]
+            normalized_point = point - center
+            normalized_W[f][p] = normalized_point[0]
+            normalized_W[f + num_frames][p] = normalized_point[1]
 
     return normalized_W
 
@@ -70,33 +62,33 @@ def solve_Q(i_hat, j_hat):
 
     offset = 0
     for frame in xrange(num_frames):
-        i_hat_row = i_hat[frame]
-        j_hat_row = j_hat[frame]
+        i_hat_1, i_hat_2, i_hat_3 = i_hat[frame]
+        j_hat_1, j_hat_2, j_hat_3 = j_hat[frame]
 
         # There probably is a 'better' way to compute each row segment
         # but this works for now
-        elem_1_1 = i_hat_row[0]**2
-        elem_1_2 = 2 * i_hat_row[0] * i_hat_row[1]
-        elem_1_3 = 2 * i_hat_row[0] * i_hat_row[2]
-        elem_1_4 = i_hat_row[1]**2
-        elem_1_5 = 2 * i_hat_row[1] * i_hat_row[2]
-        elem_1_6 = 2 * i_hat_row[2]**2
+        elem_1_1 = i_hat_1**2
+        elem_1_2 = 2 * i_hat_1 * i_hat_2
+        elem_1_3 = 2 * i_hat_1 * i_hat_3
+        elem_1_4 = i_hat_2**2
+        elem_1_5 = 2 * i_hat_2 * i_hat_3
+        elem_1_6 = 2 * i_hat_3**2
         row_1 = [elem_1_1, elem_1_2, elem_1_3, elem_1_4, elem_1_5, elem_1_6]
 
-        elem_2_1 = j_hat_row[0]**2
-        elem_2_2 = 2 * j_hat_row[0] * j_hat_row[1]
-        elem_2_3 = 2 * j_hat_row[1] * j_hat_row[2]
-        elem_2_4 = j_hat_row[1]**2
-        elem_2_5 = 2 * j_hat_row[1] * j_hat_row[2]
-        elem_2_6 = j_hat_row[2]**2
+        elem_2_1 = j_hat_1**2
+        elem_2_2 = 2 * j_hat_1 * j_hat_2
+        elem_2_3 = 2 * j_hat_1 * j_hat_3
+        elem_2_4 = j_hat_2**2
+        elem_2_5 = 2 * j_hat_2 * j_hat_3
+        elem_2_6 = j_hat_3**2
         row_2 = [elem_2_1, elem_2_2, elem_2_3, elem_2_4, elem_2_5, elem_2_6]
 
-        elem_3_1 = i_hat_row[0] * j_hat_row[0]
-        elem_3_2 = i_hat_row[0] * j_hat_row[1] + i_hat_row[1] * j_hat_row[0]
-        elem_3_3 = i_hat_row[0] * j_hat_row[2] + i_hat_row[2] * j_hat_row[0]
-        elem_3_4 = i_hat_row[1] * j_hat_row[1]
-        elem_3_5 = i_hat_row[1] * j_hat_row[2]
-        elem_3_6 = i_hat_row[2] * j_hat_row[2]
+        elem_3_1 = i_hat_1 * j_hat_1
+        elem_3_2 = (i_hat_1 * j_hat_2) + (i_hat_2 * j_hat_1)
+        elem_3_3 = (i_hat_1 * j_hat_3) + (i_hat_3 * j_hat_1)
+        elem_3_4 = (i_hat_2 * j_hat_2)
+        elem_3_5 = (i_hat_2 * j_hat_3) + (i_hat_3 * j_hat_2)
+        elem_3_6 = (i_hat_3 * j_hat_3)
         row_3 = [elem_3_1, elem_3_2, elem_3_3, elem_3_4, elem_3_5, elem_3_6]
 
         A[offset:offset + 3] = [row_1, row_2, row_3]
@@ -108,13 +100,13 @@ def solve_Q(i_hat, j_hat):
 
     # Solve for c via least squares.
     result = np.linalg.lstsq(A, b)
-    c = result[0]
+    c1, c2, c3, c4, c5, c6 = result[0]
 
     # Form C from c
     C = np.zeros((3, 3))
-    C[0] = [c[0], c[1], c[2]]
-    C[1] = [c[1], c[3], c[4]]
-    C[2] = [c[2], c[4], c[5]]
+    C[0] = [c1, c2, c3]
+    C[1] = [c2, c4, c5]
+    C[2] = [c3, c5, c6]
 
     # Solve for Q and return it.
     Q = np.linalg.cholesky(C)
@@ -142,20 +134,16 @@ def sfm(points):
     R = np.dot(R_hat, Q)
     S = np.linalg.solve(Q, S_hat)
 
-    print "R.shape: ", R.shape, "; S.shape: ", S.shape
-
     # Extract the rotation matrices from R and put them into a list, one R per
     # image.
     rotation_matricies = list()
 
-    offset = 0
     for frame in xrange(num_frames):
         rotation_matrix = np.zeros((2, R.shape[1]))
-        temp = R[offset:offset + 2]
-        print "Frame: ", frame, "temp.shape", temp.shape
-        rotation_matrix[:] = R[offset:offset + 2]
-        offset += 2
+        rotation_matrix[0] = R[frame]
+        rotation_matrix[1] = R[num_frames + frame]
         rotation_matricies.append(rotation_matrix)
+        print rotation_matrix
 
     # Return the list of R matrices and an Nx3 matrix P containing the
     # reconstructed 3D points.
